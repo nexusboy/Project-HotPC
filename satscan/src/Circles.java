1f4ad135460844637329030dd9c9839971525228
nexusboy <nexusboy@github.com>
2017-11-12 12:25:02 +0530

    Initial commit

---
+++ satscan/src/Circles.java
@@ -0,0 +1,14 @@
+
+class Circles{
+	double llh;
+	int centre;
+	int endpoint;
+	double radius;
+	
+	Circles(double llh, int centre,int endpoint, double radius) {
+		this.llh = llh;
+		this.centre = centre;
+		this.endpoint = endpoint;
+		this.radius = radius;
+	}
+}
