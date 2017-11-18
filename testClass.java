6629a97774050de9b9846054fed4b47d4cacfef6
Nikhil Teja <2014csb1011@iitrpr.ac.in>
2017-11-18 04:24:16 +0000

    Initial commit

---
+++ testClass.java
@@ -0,0 +1,13 @@
+
+public class testClass {
+public static int c  = 0 ; 
+
+public static void main(String[] args) {
+	System.out.println(getId());
+	
+	System.out.println(c);
+}
+public static int getId(){
+	return c++ ; 
+}
+}
